dcd40821e6bd4d6dae506f3fe4a7c0a233ebfe6a
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-10 21:13:37 +0900

    feat: Pass path variable to controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -30,5 +30,5 @@ Route::get('/admin/posts/example', array('as'=> 'admin.home', function () {
     return "this url is " . $url;
 }));
 
-Route::get('/posts', 'PostsController@index');
+Route::get('/posts/{id}', 'PostsController@index');
 
