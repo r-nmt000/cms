ae9d0afc3eb3338df0d36a6b21ab2d7a3d88fb37
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-12 21:56:36 +0900

    feat: use template function on blade

--- src/resources/views/contact.blade.php
+++ src/resources/views/contact.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
+@section('content')
+    <h1>Contact page</h1>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@stop
 
-    <!-- Styles -->
-</head>
-<body>
-  <div>
-      <h1>Contact Page</h1>
-  </div>
-</body>
-</html>
+@section('footer')
+
+  <script>alert('Hello visitor')</script>
+
+@stop
--- src/resources/views/post.blade.php
+++ src/resources/views/post.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-</head>
-<body>
-<div>
-    <h1>Post {{$id}}, {{$name}}, {{$password}} </h1>
-</div>
-</body>
-</html>
+@section('content')
+    <h1>Post {{$id}}, {{$name}}, {{$password}}</h1>
+@stop
